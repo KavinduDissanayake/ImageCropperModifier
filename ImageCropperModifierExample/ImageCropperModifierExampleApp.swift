d2534291ea3c805ecd3db7260886a394a9a44547
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2024-11-10 01:35:26 +0530

    Initial commit

---
+++ ImageCropperModifierExample/ImageCropperModifierExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCropperModifierExampleApp.swift
+//  ImageCropperModifierExample
+//
+//  Created by KavinduDissanayake on 2024-11-10.
+//
+
+import SwiftUI
+
+@main
+struct ImageCropperModifierExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
